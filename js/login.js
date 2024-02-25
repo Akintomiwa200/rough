98ebe532e95dd95b47a57ceafe71ad1eaef2a012
Akintomiwa Peter Adedokun <143067882+Akintomiwa200@users.noreply.github.com>
2024-02-25 22:03:28 +0100

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -2,3 +2,46 @@ function myFunction() {
     location.replace("../index2.html")
 }
 
+
+
+function next1() {
+    document.getElementById('sign').style.display = 'none';
+    var sign1Element = document.getElementById('sign1');
+    sign1Element.style.display = 'flex';
+    sign1Element.classList.remove('hidden');
+}
+
+
+function nextm2() {
+    document.getElementById('sign1').style.display = 'none';
+    var sign2Element = document.getElementById('sign2');
+    sign2Element.style.display = 'flex';
+    sign2Element.classList.remove('hidden');
+}
+
+
+
+function nextm3() {
+    document.getElementById('sign2').style.display = 'none';
+    var sign3Element = document.getElementById('sign3');
+    sign3Element.style.display = 'flex';
+    sign3Element.classList.remove('hidden');
+}
+
+
+function back1{
+
+}
+
+
+
+function back2() {
+    
+}
+
+
+function back3() {
+    
+}
+
+
